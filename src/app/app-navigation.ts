a9364730475f819cf4e18ae1a3a8cad1d8209f52
Andrés Felipe Paipilla Rojas <felipepaipilla@hotmail.com>
2025-12-15 23:24:41 -0500

    Primer commit

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,23 @@
+type NavigationItem = { path?: string, text: string, icon?: string, items?: NavigationItem[] };
+export const navigation: NavigationItem[] = [
+  {
+    text: 'Home',
+    path: '/home',
+    icon: 'home'
+  },
+  {
+    text: 'Examples',
+    icon: 'folder',
+    items: [
+      {
+        text: 'Profile',
+        path: '/profile'
+      },
+      {
+        text: 'Tasks',
+        path: '/tasks'
+      }
+    ]
+  }
+];
+
